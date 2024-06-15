86d4afc9fe53f83806820150b79179fd6f92a4ac
M. Syauqi Fadhlika <ukisyauqi@gmail.com>
2024-06-15 22:48:28 +0700

    init n finish

--- App.js
+++ App.js
@@ -1,45 +1,88 @@
-import React, { useState } from 'react'
-import Home from "../screens/home"
-import AddNote from '../screens/addNote'
-import EditNote from '../screens/editNote'
+import React, { useState } from "react";
+import Home from "./src/screens/home";
+import AddNote from "./src/screens/addNote";
+import EditNote from "./src/screens/editNote";
 
-const CurrentPageWidget = ({ currentPage, noteList, setCurrentPage }) => {
+const CurrentPageWidget = ({
+  currentPage,
+  noteList,
+  setCurrentPage,
+  addNote,
+  deleteNote,
+  editNote,
+  currentActiveNoteId,
+  setCurrentActiveNoteId,
+}) => {
   switch (currentPage) {
-    case 'home':
+    case "home":
       return (
         <Home
           noteList={noteList}
           setCurrentPage={setCurrentPage}
+          deleteNote={deleteNote}
+          setCurrentActiveNoteId={setCurrentActiveNoteId}
         />
-      )
-    case 'add':
-      return <AddNote />
-    case 'edit':
-      return <EditNote />
+      );
+    case "add":
+      return <AddNote setCurrentPage={setCurrentPage} addNote={addNote} />;
+    case "edit":
+      return (
+        <EditNote
+          setCurrentPage={setCurrentPage}
+          editNote={editNote}
+          currentActiveNoteId={currentActiveNoteId}
+        />
+      );
     default:
-      return <Home />
+      return <Home />;
   }
-}
+};
 
 const App = () => {
-  const [currentPage, setCurrentPage] = useState('home')
+  const [currentPage, setCurrentPage] = useState("home");
+  const [currentActiveNoteId, setCurrentActiveNoteId] = useState();
 
   const [noteList, setNoteList] = useState([
     {
       id: 1,
-      title: 'Note pertama',
-      desc:
-        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry',
+      title: "Note pertama",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry",
     },
-  ])
+  ]);
+
+  const addNote = (title, desc) => {
+    const id = noteList.length > 0 ? noteList[noteList.length - 1].id + 1 : 1;
+
+    setNoteList([
+      ...noteList,
+      {
+        id,
+        title: title,
+        desc: desc,
+      },
+    ]);
+  };
+
+  const deleteNote = (id) => {
+    setNoteList(noteList.filter((note) => note.id !== id));
+  };
+
+  const editNote = (id, title, desc) => {
+    setNoteList(noteList.map(note => note.id === id ? { id, title, desc } : note));
+  };
 
   return (
     <CurrentPageWidget
       currentPage={currentPage}
       setCurrentPage={setCurrentPage}
       noteList={noteList}
+      addNote={addNote}
+      deleteNote={deleteNote}
+      editNote={editNote}
+      currentActiveNoteId={currentActiveNoteId}
+      setCurrentActiveNoteId={setCurrentActiveNoteId}
     />
-  )
-}
+  );
+};
 
-export default App
+export default App;
